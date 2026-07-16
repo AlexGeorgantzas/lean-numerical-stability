ee7f4ad699a068f7250d602a10ae3c9bb5bf4eb8
Muhammad Fetrat <m2fetrat@watgpu108.watgpu-domain.cs.uwaterloo.ca>
2026-07-16 19:57:42 +0000

    Split 3A: prove unequal-block LU criterion

--- LeanFpAnalysis/FP/Algorithms.lean
+++ LeanFpAnalysis/FP/Algorithms.lean
@@ -54,6 +54,7 @@ import LeanFpAnalysis.FP.Algorithms.LU.TridiagonalRecurrence
 import LeanFpAnalysis.FP.Algorithms.LU.SpecialMatrices
 import LeanFpAnalysis.FP.Algorithms.LU.Doolittle
 import LeanFpAnalysis.FP.Algorithms.LU.BlockLU
+import LeanFpAnalysis.FP.Algorithms.LU.BlockLUVarying
 import LeanFpAnalysis.FP.Algorithms.LU.BlockLUSPDSourceClosure
 import LeanFpAnalysis.FP.Algorithms.LU.BlockLUFirstOrderFamilies
 import LeanFpAnalysis.FP.Algorithms.LU.BlockLUComputationSourceClosure
